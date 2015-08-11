04092c42ed500120bb74dc6be7bc3706650e862a
Никита Соколов <faucct@gmail.com>
2015-08-11 23:14:10 +0300

    define_method

--- lib/virtus/attribute/collection_accessor.rb
+++ lib/virtus/attribute/collection_accessor.rb
@@ -2,16 +2,20 @@ module Virtus
   class Attribute
     module CollectionAccessor
       def self.extended(descendant)
-        primitive = Class.new(descendant.primitive).tap do |klass|
-          klass.include(CoercibleCollection)
-          klass.instance_variable_set('@member_type', descendant.member_type)
-        end
+        primitive = Class.new(descendant.primitive)
+        member_type = descendant.member_type
+        primitive.send(:define_method, :coerce) { |value| member_type.coerce value }
+        primitive.include(CoercibleCollection)
         descendant.instance_variable_set('@primitive', primitive)
       end
 
       module CoercibleCollection
         def <<(value)
-          super self.class.instance_variable_get('@member_type').coerce value
+          super(coerce value)
+        end
+
+        def []=(key, value)
+          super(key, coerce(value))
         end
       end
     end
