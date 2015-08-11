cb4ad900078780ec4a44366a055285d2decb4018
Никита Соколов <faucct@gmail.com>
2015-08-11 19:40:38 +0300

    Virtus::Attribute::CollectionAccessor - any collection

--- lib/virtus/attribute/collection_accessor.rb
+++ lib/virtus/attribute/collection_accessor.rb
@@ -1,13 +1,19 @@
 module Virtus
   class Attribute
     module CollectionAccessor
+      @@collections = {}
+      @@mutex = Mutex.new
+
       def get(instance)
-        Array.new(super).tap do |collection|
+        value = super
+        @@mutex.synchronize do
+          @@collections[value.class] ||= Class.new(value.class).tap { |klass| klass.include(CoercibleCollection) }
+        end.new(value).tap do |collection|
           collection.instance_variable_set('@member_type', member_type)
         end
       end
 
-      class Array < ::Array
+      module CoercibleCollection
         def <<(value)
           if value.kind_of? ::Hash
             super @member_type.coerce value
